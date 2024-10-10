42aa317d25c2cb215cdd370c85df5433a02c667a
Bruyz <brunastephanyliove@gmail.com>
2024-10-10 09:52:52 -0300

    boaa

--- scr/navigations/drawer.navigation.tsx
+++ scr/navigations/drawer.navigation.tsx
@@ -1,9 +1,10 @@
 import { createDrawerNavigator, DrawerNavigationProp } from '@react-navigation/drawer';
 import { colors } from '../styles/colors';
-import { Ionicons, FontAwesome, MaterialCommunityIcons, Feather} from '@expo/vector-icons';
+import { Ionicons, FontAwesome, MaterialCommunityIcons, Feather, Entypo} from '@expo/vector-icons';
 import { TabNavigation } from "./tab.navigation";
 import { Camera } from "../screens/Camera"
 import { Imagens } from "../screens/Imagens"
+import { LocationMap } from "../screens/LocationMap"
 import React from 'react';
 import { QrCode } from '../screens/QrCode';
 
@@ -12,6 +13,7 @@ type DrawerParamList = {
     Camera: undefined
     Imagem: undefined
     QrCode: undefined
+    LocationMap: undefined
 }
 type DrawerScreenNavigationProp = DrawerNavigationProp<DrawerParamList, 'Tab'>
 export type DrawerTypes = {
@@ -31,25 +33,31 @@ export function DrawerNavigation() {
                 drawerLabel: 'Perfil',
                 headerTitle: 'Perfil',
                 drawerIcon: ()=> (
-                    <Ionicons name="person" size={24} color={colors.white} />
+                    <Ionicons name="person" size={24} color={colors.primary} />
                 ),
             }} />
             <Drawer.Screen name='Camera' component={Camera} 
             options={{
                 drawerIcon: ()=> (
-                    <Feather name="camera" size={24} color="white" />
+                    <Feather name="camera" size={24} color={colors.primary} />
                 ),
             }} />
             <Drawer.Screen name='Imagem' component={Imagens} 
             options={{
                 drawerIcon: ()=> (
-                    <FontAwesome name="picture-o" size={24} color={colors.white} />
+                    <FontAwesome name="picture-o" size={24} color={colors.primary} />
                 ),
             }} />
              <Drawer.Screen name='QrCode' component={QrCode} 
             options={{
                 drawerIcon: ()=> (
-                    <MaterialCommunityIcons name="qrcode-scan" size={24} color={colors.white} />
+                    <MaterialCommunityIcons name="qrcode-scan" size={24} color={colors.primary} />
+                ),
+            }} />
+              <Drawer.Screen name='Localização' component={LocationMap} 
+            options={{
+                drawerIcon: ()=> (
+                    <Entypo name="location" size={24} color={colors.primary} />
                 ),
             }} />
         </Drawer.Navigator>
--- scr/screens/index.ts
+++ scr/screens/index.ts
@@ -1,2 +1,3 @@
 export {Login as ScreenLogin} from "./Login"
 export { Register as ScreenRegister } from "./Register"
+export { LocationMap as ScreenLocation } from "./LocationMap"
